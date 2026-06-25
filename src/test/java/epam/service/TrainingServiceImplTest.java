c83f0f5418ce882a7e7aa73fd14f301e8d4a9cbb
aleksandr2110 <utel200519@gmail.com>
2026-06-25 09:56:57 +0300

    fix test

--- src/test/java/epam/service/TrainingServiceImplTest.java
+++ src/test/java/epam/service/TrainingServiceImplTest.java
@@ -3,8 +3,6 @@ package epam.service;
 import epam.domain.InnerDataTraining;
 import epam.domain.Training;
 import epam.repository.TrainingRepository;
-import epam.request.TraineeRequest;
-import epam.request.TrainerRequest;
 import epam.request.TrainingRequest;
 import epam.service.impl.TrainingServiceImpl;
 import org.junit.jupiter.api.Test;
@@ -13,7 +11,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 
@@ -33,17 +30,6 @@ public class TrainingServiceImplTest {
     @Test
     void shouldSaveTraining() {
         //GIVEN
-        /*var traineeRequest = new TraineeRequest();
-        traineeRequest.setFirstName("Sabrina");
-        traineeRequest.setLastName("Ogiy");
-        traineeRequest.setDateOfBirth(LocalDate.of(1991, 2, 2));
-        traineeRequest.setAddress("79 Nikolska st");
-
-        var trainerRequest = new TrainerRequest();
-        trainerRequest.setFirstName("Konstantin");
-        trainerRequest.setLastName("Rubak");
-        trainerRequest.setSpecialization("TypeScript");
-        trainerRequest.setActive(true); */
 
         Training training = new Training();
         training.setTrainingType("C#");
