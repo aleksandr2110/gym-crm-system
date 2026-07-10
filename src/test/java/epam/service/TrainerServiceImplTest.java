589bdf2ab263f22937ea3c54826a3a72d8537654
aleksandr2110 <utel200519@gmail.com>
2026-07-10 17:21:17 +0300

    fixed TrainerServiceImplTest

--- src/test/java/epam/service/TrainerServiceImplTest.java
+++ src/test/java/epam/service/TrainerServiceImplTest.java
@@ -1,13 +1,19 @@
 package epam.service;
 
-import epam.domain.Trainer;
+import epam.domain.*;
+import epam.repository.TraineeRepository;
 import epam.repository.TrainerRepository;
+import epam.repository.TrainingRepository;
 import epam.service.impl.TrainerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -16,8 +22,14 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 public class TrainerServiceImplTest {
 
-    /*@Mock
+    @Mock
+    private TraineeRepository traineeRepository;
+    @Mock
     private TrainerRepository trainerRepository;
+    @Mock
+    private TrainingRepository trainingRepository;
+    @Mock
+    private TrainingTypeService trainingTypeService;
     @InjectMocks
     private TrainerServiceImpl trainerService;
 
@@ -25,29 +37,42 @@ public class TrainerServiceImplTest {
     void shouldSaveTrainer() {
         //GIVEN
         Long userId = 1L;
+        var trainee = new Trainee();
+        trainee.setId(userId);
+
+        var training = new Training();
+        training.setId(userId);
+        training.setTrainingName("PHP");
+
         var trainerRequest = new Trainer();
         trainerRequest.setFirstName("Sergey");
         trainerRequest.setLastName("Hofman");
-        trainerRequest.setSpecialization("PHP");
-        trainerRequest.setActive(true);
 
-        var trainer = new Trainer();
-        trainer.setUserId(userId);
-        trainer.setFirstName("Sergey");
-        trainer.setLastName("Hofman");
-        trainer.setSpecialization("PHP");
-        trainer.setActive(true);
+        var trainingType = new TrainingType();
+        trainingType.setTrainingTypeName(TrainingTypeName.getByName("PHP"));
+
+        trainerRequest.setSpecialization(trainingType);
+        trainerRequest.setIsActive(true);
+        trainerRequest.setTrainees(Arrays.asList(trainee));
+        Set<Training> trainingSet = new LinkedHashSet<>();
+        trainingSet.add(training);
+        trainerRequest.setTrainings(trainingSet);
+
+        var newTrainer = trainerRequest;
 
         //WHEN
-        when(trainerRepository.save(any())).thenReturn(trainer);
-        var createdTrainee = trainerService.create(trainerRequest);
+        when(trainingTypeService.findByName(any())).thenReturn(trainingType);
+        when(traineeRepository.findById(any())).thenReturn(trainee);
+        when(trainingRepository.findTrainingById(any())).thenReturn(training);
+        when(trainerRepository.save(any())).thenReturn(newTrainer);
+        var createdTrainer = trainerService.save(trainerRequest);
 
         //THEN
-        assertNotNull(createdTrainee);
-        assertEquals(trainerRequest.getFirstName(), createdTrainee.getFirstName());
-        assertEquals(trainerRequest.getLastName(), createdTrainee.getLastName());
-        assertEquals(trainerRequest.getSpecialization(), createdTrainee.getSpecialization());
-        assertTrue(createdTrainee.isActive());
+        assertNotNull(createdTrainer);
+        assertEquals(trainerRequest.getFirstName(), createdTrainer.getFirstName());
+        assertEquals(trainerRequest.getLastName(), createdTrainer.getLastName());
+        assertEquals(trainerRequest.getSpecialization(), createdTrainer.getSpecialization());
+        assertTrue(createdTrainer.getIsActive());
         verify(trainerRepository, times(1)).save(any());
     }
 
@@ -55,76 +80,88 @@ public class TrainerServiceImplTest {
     void shouldUpdateTrainer() {
         //GIVEN
         Long userId = 1L;
+
+        var training = new Training();
+        training.setId(userId);
+        training.setTrainingName("PHP");
+
+        var trainingType = new TrainingType();
+        trainingType.setTrainingTypeName(TrainingTypeName.getByName("PHP"));
+
         var trainerRequest = new Trainer();
-        trainerRequest.setUserId(userId);
+        trainerRequest.setId(userId);
         trainerRequest.setFirstName("Alexandr");
         trainerRequest.setLastName("Kirichenko");
-        trainerRequest.setSpecialization("PHP");
-        trainerRequest.setActive(true);
+        trainerRequest.setSpecialization(trainingType);
+        trainerRequest.setIsActive(true);
 
         var currentTrainer = new Trainer();
-        currentTrainer.setUserId(userId);
+        currentTrainer.setId(userId);
         currentTrainer.setFirstName("Rerg");
         currentTrainer.setLastName("Grill");
-        currentTrainer.setSpecialization("PHP");
-        currentTrainer.setActive(true);
+        currentTrainer.setSpecialization(trainingType);
+        currentTrainer.setIsActive(true);
 
         var updatedTrainer = new Trainer();
-        updatedTrainer.setUserId(trainerRequest.getUserId());
+        updatedTrainer.setId(trainerRequest.getId());
         updatedTrainer.setFirstName(trainerRequest.getFirstName());
         updatedTrainer.setLastName(trainerRequest.getLastName());
         updatedTrainer.setSpecialization(trainerRequest.getSpecialization());
-        updatedTrainer.setActive(trainerRequest.isActive());
+        updatedTrainer.setIsActive(trainerRequest.getIsActive());
 
         //WHEN
-        when(trainerRepository.select(userId)).thenReturn(currentTrainer);
-        when(trainerRepository.update(any())).thenReturn(updatedTrainer);
-        var updatedTrainerById = trainerService.update(trainerRequest, userId);
+        when(trainerRepository.findById(userId)).thenReturn(currentTrainer);
+        when(trainerRepository.updateProfile(currentTrainer)).thenReturn(updatedTrainer);
+        var updatedNewTrainer = trainerService.updateProfile(trainerRequest, userId);
 
         //THEN
-        assertNotNull(updatedTrainerById);
-        assertEquals(trainerRequest.getFirstName(), updatedTrainerById.getFirstName());
-        assertEquals(trainerRequest.getLastName(), updatedTrainerById.getLastName());
-        assertEquals(trainerRequest.getSpecialization(), updatedTrainerById.getSpecialization());
-        assertTrue(updatedTrainerById.isActive());
+        assertNotNull(updatedNewTrainer);
+        assertEquals(trainerRequest.getFirstName(), updatedNewTrainer.getFirstName());
+        assertEquals(trainerRequest.getLastName(), updatedNewTrainer.getLastName());
+        assertEquals(trainerRequest.getSpecialization(), updatedNewTrainer.getSpecialization());
+        assertTrue(updatedNewTrainer.getIsActive());
     }
 
     @Test
     void shouldThrowExceptionWhenUpdateTrainer() {
         //GIVEN
         Long userId = 1L;
-        Trainer empty = null;
+        Trainer emptyTrainee = null;
         var trainerRequest = new Trainer();
+        trainerRequest.setId(userId);
 
         //WHEN
-        when(trainerRepository.select(userId)).thenReturn(empty);
+        when(trainerRepository.findById(userId)).thenReturn(emptyTrainee);
 
         //THEN
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            trainerService.update(trainerRequest, userId);
+            trainerService.updateProfile(trainerRequest, userId);
         });
-        assertEquals("User with id: " + userId + " not found!", exception.getMessage());
+        assertEquals("Trainee with id: " + userId + " not found!", exception.getMessage());
     }
 
     @Test
     void shouldSelectTrainerById() {
         //GIVEN
         Long userId = 1L;
+
         var trainer = new Trainer();
         trainer.setFirstName("Rerg");
         trainer.setLastName("Grill");
-        trainer.setSpecialization("JavaScript");
-        trainer.setActive(false);
+        var trainingType = new TrainingType();
+        trainingType.setTrainingTypeName(TrainingTypeName.getByName("Java"));
+        trainer.setSpecialization(trainingType);
+        trainer.setIsActive(false);
 
         //WHEN
-        when(trainerRepository.select(userId)).thenReturn(trainer);
-        var selectedTrainerById = trainerService.select(userId);
+        when(trainerRepository.findById(userId)).thenReturn(trainer);
+        var selectedTrainerById = trainerService.findById(userId);
 
         //THEN
         assertNotNull(selectedTrainerById);
         assertEquals(trainer.getFirstName(), selectedTrainerById.getFirstName());
         assertEquals(trainer.getLastName(), selectedTrainerById.getLastName());
         assertEquals(trainer.getSpecialization(), selectedTrainerById.getSpecialization());
-        assertFalse(selectedTrainerById.isActive());
-    }*/
+        assertFalse(selectedTrainerById.getIsActive());
+    }
 }
